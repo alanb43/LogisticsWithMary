ded1c183b05cf986d3680c7fe75a2011bff103ad
alanb43 <ab43work@gmail.com>
2022-03-06 16:25:31 -0500

    fixed documentation

--- logisticswithmary/__init__.py
+++ logisticswithmary/__init__.py
@@ -4,7 +4,7 @@ import flask
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 
-# Read settings from config module (swetter/config.py)
+# Read settings from config module (logisticswithmary/config.py)
 app.config.from_object('logisticswithmary.config')
 
 # Overlay settings read from a Python file whose path is set in the environment
@@ -12,7 +12,7 @@ app.config.from_object('logisticswithmary.config')
 # Docs: http://flask.pocoo.org/docs/latest/config/
 #
 # EXAMPLE:
-# $ export SWETTER_SETTINGS=secret_key_config.py
+# $ export LOGISTICSWITHMARY_SETTINGS=secret_key_config.py
 app.config.from_envvar('LOGISTICSWITHMARY_SETTINGS', silent=True)
 
 # Tell our app about views and model.  This is dangerously close to a
